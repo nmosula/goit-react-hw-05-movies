c43aa3d39709a9610418d5fc8f6667815e7cc93c
Nazar Mosula <nmosula@gmail.com>
2023-03-20 20:38:33 +0200

    Added fetchMovieCast

--- src/Api.js
+++ src/Api.js
@@ -7,28 +7,47 @@ const API_KEY = "bf90c64edd4fb3718c72a5077448d3a4";
 export async function fetchTrandingMovies () {
   const response = await axios.get(`${BASE_URL}/trending/movie/day?api_key=${API_KEY}`);
   
-    const movies = response.data.results.map(
-        ({ id, title }) => {
-            return {
-                id,
-                title,
-            }
-        }
+    const movies = response.data.results.map(({ id, title }) => {
+      return {
+        id,
+        title,
+      }
+    }
   )
+
   return { movies };
 };
 
 export async function fetchMovieById(movieId) {
   const response = await axios.get(`${BASE_URL}/movie/${movieId}?api_key=${API_KEY}`);
+
   const { title, poster_path, overview, genres, vote_average, release_date } = response.data;
 
   return {
-    title,
-    posterPath: isAvailableImage(poster_path),
-    overview,
-    genres: genres.map(genre => genre.name).join(', '),
-    vote: vote_average.toFixed(1)*10,
-    year: release_date.slice(0, 4),
+      title,
+      posterPath: isAvailableImage(poster_path),
+      overview,
+      genres: genres.map(genre => genre.name).join(', '),
+      vote: vote_average.toFixed(1)*10,
+      year: release_date.slice(0, 4),
   };
+};
+
+export async function fetchMovieCast(movieId) {
+  const response = await axios.get(`${BASE_URL}/movie/${movieId}/credits?api_key=${API_KEY}`);
+  // console.log(response.data.cast);
+
+  const movieCast = response.data.cast.map(
+    ({ id, name, profile_path, character }) => {
+      return {
+        id,
+        name,
+        profile_path: isAvailableImage(profile_path),
+        character,
+      }
+    }
+  );
+
+  return movieCast;
 
 };
