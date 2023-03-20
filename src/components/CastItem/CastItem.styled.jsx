42931bdf7f1d24cd25b950f180a093e313281382
Nazar Mosula <nmosula@gmail.com>
2023-03-20 20:39:56 +0200

    Created CastItem

---
+++ src/components/CastItem/CastItem.styled.jsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const ItemCard = styled.li`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  list-style: none;
+  height: auto;
+  width: calc((100% - 20px) / 2);
+  border-radius: 10px;
+  background-color: #000;
+  @media screen and (min-width: 768px) {
+    width: calc((100% - 60px) / 4);
+  }
+  @media screen and (min-width: 1280px) {
+    width: calc((100% - 100px) / 6);
+  }
+`;
+export const ItemPhoto = styled.img`
+  width: 100%;
+  height: 250px;
+  object-fit: cover;
+  border-radius: 10px;
+`;
+
+export const ItemInfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  padding: 10px;
+`;
+
+export const ItemInfoText = styled.p`
+  margin: 0;
+  color: #ebebeb;
+  font-size: 16px;
+`;
+
+export const ItemSpanText = styled.span`
+  font-weight: 700;
+`
