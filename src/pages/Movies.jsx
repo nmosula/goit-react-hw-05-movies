9782d5cd3bd09fb8dc1468dc4a9fc3318f844900
Nazar Mosula <nmosula@gmail.com>
2023-03-21 21:51:26 +0200

    Update Page Movies

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,13 +1,66 @@
-// import { getMovies } from "Api";
-// import { MoviesList } from "components/MoviesList";
+import { useState, useEffect } from 'react';
+import { fetchMoviesByQuery } from "Api";
+import Loader from "components/Loader";
+import { MoviesList } from "components/MoviesList";
+import { useSearchParams } from 'react-router-dom';
+import Notiflix from 'notiflix';
+import SearchForm from 'components/SearchForm';
 
 export const Movies = () => {
 
+    const [movies, setMovies] = useState('[]');
+    const [status, setStatus] = useState('idle');
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [query, setQuery] = useState(searchParams.get('query'));
+    
+    useEffect(() => {
+        if (!query) return;
+        
+        const getMoviesByQuery = async () => {
+        setStatus('pending');
+            try {
+                const { movies, totalResults } = await fetchMoviesByQuery();
+
+                if (totalResults === 0) {
+                    Notiflix.Notify.warning(`Нажаль, нічого не знайдено, повторіть пошук`);
+
+                    setMovies([]);
+                    setQuery(null);
+                }
+                
+                setMovies(movies);
+                setStatus('resolved');          
+            }
+            catch (error) {
+                console.error(error);
+            }   
+        }
+
+        getMoviesByQuery();
+    }, [query]);
+
+
+    const handleSearch = (searchValue) => {
+        if (!searchValue) {
+            Notiflix.Notify.warning(`Будь ласка, введіть пошуковий запит`);
+            return;
+        }
+
+        if (query === searchValue) {
+            Notiflix.Notify.warning(`Будь ласка, введіть інший пошуковий запит`);
+            return;
+        }
+
+        setQuery(searchValue);
+        setSearchParams({ query: searchValue });
+        setStatus('idle');
+    }
+    
     return (
         <main>
-            <p>
-                Movies
-            </p>
+            <SearchForm onSubmit={handleSearch} />
+            {status === 'pending' && <Loader />}
+            {status === 'resolved' && <MoviesList movies={movies} />}
         </main>
     )
 }
